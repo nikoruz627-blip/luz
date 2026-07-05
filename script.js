ad99aefead32af6f93faa41a0c4cb2ddc7077b38
luz <nikoruz627@gmail.com>
2026-07-05 17:36:47 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // ==========================
-// ANIMACIÓN DE BOTONES
+// ANIMACIÓN DE ENTRADA
 // ==========================
 
 const buttons = document.querySelectorAll(".link-btn");
@@ -9,22 +9,26 @@ buttons.forEach((button, index) => {
     button.style.transform = "translateY(20px)";
 
     setTimeout(() => {
-        button.style.transition = "all 0.5s ease";
+        button.style.transition = "all .5s ease";
         button.style.opacity = "1";
         button.style.transform = "translateY(0)";
-    }, 200 + (index * 150));
+    }, 250 + (index * 150));
 });
 
 // ==========================
-// EFECTO CLICK
+// EFECTO AL HACER CLICK
 // ==========================
 
 buttons.forEach(button => {
+
     button.addEventListener("click", () => {
-        button.style.transform = "scale(0.97)";
+
+        button.style.transform = "scale(.97)";
 
         setTimeout(() => {
             button.style.transform = "";
         }, 150);
+
     });
+
 });
